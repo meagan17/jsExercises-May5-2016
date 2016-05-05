972ea76e57b8a76e437b26c515c8d547813a86cc
meagan17 <mramo4555@withs.org>
2016-05-05 14:55:37 +0000

    first sub

--- for-loop-exercises.js
+++ for-loop-exercises.js
@@ -25,7 +25,11 @@ function example() {
   Inside the body, create a for loop that repeats 5 times
   Inside the for loop, call run()
 */
-
+function looper() {
+ for (var i = 0; i < 5; i++){
+   run();
+ } 
+}
 
 /*
   Create a function named 'inception' with one parameter
@@ -38,7 +42,11 @@ function example() {
     inception(4) -> run() is called four times
     inception(0) -> run() should not be called
 */
-
+function inception(dream){
+  for (var i = 0; i < dream; i++){
+    run();
+  }
+}
 
 /*
   Create a function named 'redLightGreenLight' with two parameters
@@ -51,7 +59,16 @@ function example() {
     redLightGreenLight(4, true) -> run() is called four times
     redLightGreenLight(0, false) -> stop() should not be called
 */
-
+function redLightGreenLight(red,green){
+  for (var i = 0; i < red; i++){
+    if(green){
+      run();
+    }
+    else{
+      stop();
+    }
+  }
+}
 
 /*
   Create a function named 'range' with two parameters
@@ -66,7 +83,11 @@ function example() {
     range(3, 6) -> run() is called thrice
     range(2, 0) -> run() should not be called
 */
-
+function range(distance,length){
+  for (var i = distance; i < length; i++){
+    run();
+  }
+}
 
 /*
   Create a function named 'rangeAdv' with three parameters
@@ -84,3 +105,8 @@ function example() {
     rangeAdv(2, 0, 5) -> run() should not be called
     rangeAdv(0, 2, 5) -> run() is called once
 */
+function rangeAdv(short,long,halfway){
+  for (var i = short; i < long; i +=halfway){
+    run();
+  }
+}
